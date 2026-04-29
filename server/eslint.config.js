dadb416ba0da2583a5d5f9f0bd8d38eac20ca110
Ezo-mas <aluskarstas@gmail.com>
2026-04-29 19:14:18 +0300

    ci: add lint/tsc/test workflow

---
+++ server/eslint.config.js
@@ -0,0 +1,20 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([
+  globalIgnores(['dist', 'generated', 'prisma/migrations']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [js.configs.recommended, tseslint.configs.recommended],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+        ...globals.es2022,
+      },
+    },
+  },
+])
